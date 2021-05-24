d06f7907f5ae854dceee35123d3900dc9ada6102
Sharon Leira Rojas <84730310+sleira@users.noreply.github.com>
2021-05-24 20:16:21 +0200

    Add files via upload

---
+++ validarnumero.py
@@ -0,0 +1,9 @@
+###Introducir un numero por teclado####
+x=int(input('Introducir el numero: '))
+if x<0: 
+ print('numero negativo')
+elif x==0: 
+   print('numero igual Zero')
+elif x==1: 
+   print('numero igual a Uno')
+else: print('Numero mayor a uno')
